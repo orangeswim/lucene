44a4c2fd6f9428fa687d1a9896ee8750fc207343
Cyprian Bergonia <orangeswimstudios@gmail.com>
2022-05-18 01:33:31 -0500

    fix test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'eslint:recommended',
+  parser: "babel-eslint",
+  extends: "eslint:recommended",
   env: {
-    node: true
+    node: true,
   },
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'no-useless-escape': 0
-  }
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    //quotes: ['error', 'single'],
+    semi: ["error", "always"],
+    "no-useless-escape": 0,
+  },
 };
